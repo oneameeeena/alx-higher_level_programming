1e044e176414b3df294ca9cdc191e78424e285a3
oneameeeena <aminaouaqqaourahou@gmail.com>
2024-03-30 20:43:33 -0700

    py

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-import sys 
-sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
-exit(1)
+
+import sys
+
+message = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
+
+sys.stderr.write(message)
+sys.exit(1)
