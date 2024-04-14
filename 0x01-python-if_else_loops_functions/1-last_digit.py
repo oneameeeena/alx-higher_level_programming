5bee57aa736a79edb781ab4d16fe9ce86fe17ef0
oneameeeena <aminaouaqqaourahou@gmail.com>
2024-04-14 06:56:49 -0700

    py1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10
-
 if (number > 0):
-    print("Last digit of {:d} is {:d}".format(number, last_digit))
-elif (number < 0):
-    print("Last digit of {:d} is {:d}".format(number, last_digit * -1))
+    last = number % 10
 else:
-    print("Last digit of {:d} is {:d}".format(number, last_digit))
-
-if (last_digit > 5):
-    print("and is greater than 5")
-elif (last_digit == 0):
-    print("and is 0")
+    last = number % -10
+if (last > 5):
+    print(f"Last digit of {number} is {last} and is greater than 5")
+elif (last < 6 and last != 0):
+    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
 else:
-    print("and is less than 6 and not 0")
+    print(f"Last digit of {number} is {last} and is 0")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if i == ord('e') or i == ord('q'):
-        continue
-    print("{:c}".format(i), end="")
-print()
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{}".format(chr(i)), end="")
