98750b20e205b9296b1813c5c120d3aea561b29f
oneameeeena <aminaouaqqaourahou@gmail.com>
2024-04-17 04:27:35 -0700

    0A.PY

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
-'''Module for inherits_from method.'''
+'''Module for is_true_subclass method.'''
 
-def inherits_from(obj, a_class):
-    '''Determines if an object is a true subclass of a class.'''
+from typing import Type, Any
+
+def is_true_subclass(obj: Any, a_class: Type) -> bool:
+    '''Determines if an object is a true subclass of a class.
+
+    A true subclass is a subclass that is not the same as the class itself.
+    This is important to distinguish because some methods, such as isinstance(),
+    will return True for both the class and its subclasses.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to check against.
+
+    Returns:
+        True if obj is a true subclass of a_class, False otherwise.
+    '''
     return isinstance(obj, a_class) and type(obj) != a_class
